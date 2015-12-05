bd26f75163681d436acc709c9102116953dcc946
Kushal Byatnal <kbyatnal@gmail.com>
2015-12-05 17:36:51 -0500

    PHP files

---
+++ php/autocomplete.php
@@ -0,0 +1,23 @@
+<?php
+
+    $term = trim($_GET['term']);
+    $return_arr = array();
+
+	$db = pg_connect("dbname=us_congress host=localhost user=postgres password=kushal941")
+    				or die('Could not connect: ' . pg_last_error());
+
+	$sql = "SELECT (first_name || ' ' || last_name) AS FullName FROM persons WHERE first_name ILIKE '".$term."%' OR last_name ILIKE '".$term."%' ";
+
+	$result = pg_query($db, $sql) or die('Query failed: ' . pg_last_error());
+
+		while ($line = pg_fetch_row($result)) {
+    				array_push($return_arr, $line[0]);
+			}
+
+	pg_free_result($result);
+
+echo json_encode($return_arr);
+
+pg_close($db);
+
+?>
