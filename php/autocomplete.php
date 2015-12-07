59e702c4d3f995ec57d3af619832154850cc197a
Thomas Bagley <thb7@duke.edu>
2015-12-07 02:41:43 -0500

    php updates

--- php/autocomplete.php
+++ php/autocomplete.php
@@ -4,12 +4,16 @@
     $termSplit = explode(" ", $term);
     $return_arr = array();
 
-	$db = pg_connect("dbname=us_congress host=localhost user=postgres password=kushal941")
+	$db = pg_connect("dbname=us_congress host=localhost user=postgres password=Bd3nM2!Vg27aJ!0")
     				or die('Could not connect: ' . pg_last_error());
 
   if(count($termSplit) == 1){
-    $sql = "SELECT (first_name || ' ' || last_name) AS FullName FROM persons WHERE first_name ILIKE '%".$term."%' OR last_name ILIKE '%".$term."%' ";
+    $sql = "SELECT DISTINCT (first_name || ' ' || last_name) AS FullName
+    FROM persons, person_roles
+    WHERE (first_name ILIKE '%".$term."%' OR last_name ILIKE '%".$term."%') AND
+          (persons.id = person_roles.person_id) AND (EXTRACT(YEAR from start_date) >= 2011 AND EXTRACT(YEAR from start_date) <= 2014)";
   }
+
   elseif(count($termSplit) == 2){
     $sql = "SELECT (first_name || ' ' || last_name) AS FullName FROM persons WHERE first_name ILIKE '%".$termSplit[0]."%' AND last_name ILIKE '".$termSplit[1]."%' ";
   }
